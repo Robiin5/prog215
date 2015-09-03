3f3d7708947454aa7b67a52f77c018e403c0ca22
Robin Bleur Pallasma <robinbleur.pallasma@tigu.hk.tlu.ee>
2015-09-03 12:08:17 +0300

    getter

--- algus/Asukoht.java
+++ algus/Asukoht.java
@@ -1,4 +1,16 @@
 class Asukoht{
-   double lat;
-   double lng;
+   public Asukoht(double laiuskraad, double pikkuskraad){
+      lat=kraadParajaks(laiuskraad);
+	  lng=kraadParajaks(pikkuskraad);
+   }
+   private double kraadParajaks(double kraad){
+      while(kraad>180) {kraad-=360;}
+      while(kraad<-180){kraad+=360;}
+	  return kraad;
+   }
+   public @Override String toString(){ return "("+lat+","+lng+")";}
+   private double lat;
+   private double lng;
+   public double getLat(){return lat;}
+   public double getLng(){return lng;}
 }
